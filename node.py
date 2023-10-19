61ab3f637cde92d49fa334e4c30dbb43f38cae2a
Lucas Butler <08lbutler31@gmail.com>
2023-10-19 10:57:30 -0700

    Node class V1

---
+++ node.py
@@ -0,0 +1,15 @@
+# Lucas Butler
+
+# Code for a node class and a node list generator
+class Node():
+
+    score = None
+    coop_prob = float()
+
+    def __init__(self, score, coop_prob):
+        self.score=score
+        self.coop_prob=coop_prob
+
+    def __repr__(self):
+        print(f"({self.score}, {self.coop_prob})")()
+    
