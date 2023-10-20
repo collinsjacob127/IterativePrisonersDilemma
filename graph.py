ebda71dc386a60d0e2db5142d1bc028ca12323ac
collinsjacob127 <collinsjacob127@gmail.com>
2023-10-20 01:59:35 -0700

    Preliminary stage

--- graph.py
+++ graph.py
@@ -1,7 +1,7 @@
 # Jacob Collins
 
 from node import Node
-from networkx import Graph
+from networkx import Graph, configuration_model
 from numpy.random import choice
 
 
@@ -10,25 +10,26 @@ from numpy.random import choice
 # @param coop_vals: list of possible starting coop_probs for prisoners
 # @param coop_odds: probability of a node having the coop_prob assosciated with
 #                   the corresponding index in coop_vals
-class configModel():
-
-    network = Graph()
-
-    def __init__(self, deg_seq, init_score=0, coop_vals=[1, 0], coop_odds=[0.5, 0.5]):
-        # Initialize Nodes into Network
-        for _ in range(len(deg_seq)):
-            self.network.add_node(Node(score=init_score, coop_prob=choice(coop_vals, 1, p=coop_odds)))
-        print(self.network.nodes())
-        # Generate List from Degree Distribution
-        config_list = [[u] * deg_seq[i] for i, u in enumerate(self.network.nodes())]
-        print(config_list)
-        # Add random edges (no self-loops)
-        while len(config_list) > 0:
-            pair = choice(config_list, 2, replace=False)
-            if pair[0] == pair[1]:
-                continue
-            self.network.add_edge(pair[0], pair[1])
+def configModel(deg_seq, init_score=0, coop_vals=[1, 0], coop_odds=[0.5, 0.5]):
+    network = configuration_model(deg_seq)
+    # Initialize Nodes into Network
+    for i in range(len(deg_seq)):
+        u = Node(score=init_score, coop_prob=choice(coop_vals, 1, p=coop_odds))
+        network.add_node(i,data=u)
+    print(f'Nodes: {network.nodes}')
+    # Generate List from Degree Distribution
+    config_list = []
+    for i, u in enumerate(network.nodes()):
+        config_list += list([u] * deg_seq[i])
+    print(config_list)
+    # Add random edges (no loops)
+    while len(config_list) > 0:
+        print(f'Config list: {config_list}')
+        pair = choice(config_list, 2, replace=False)
+        if pair[0] == pair[1]:
+            continue
+        network.add_edge(pair[0], pair[1])
+        config_list.remove(pair[0])
+        config_list.remove(pair[1])
         
-    def __repr__(self):
-        print(f'Nodes:\n{self.network.nodes()}\nEdges:\n{self.network.edges()}')
     
