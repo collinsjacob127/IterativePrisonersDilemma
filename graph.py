de52a503030d4c442cd011788be2f051ed5b5233
Jacob Collins <44860144+collinsjacob127@users.noreply.github.com>
2023-10-24 11:42:39 -0700

    Update comments and add tag as optional parameter

--- graph.py
+++ graph.py
@@ -6,17 +6,19 @@ from numpy.random import choice
 #import networkx as nx
 
 
-# Function for generating a network of agents using config model
-# @param deg_seq: 1d list of the degree for node u in range(len(deg_seq))
+# Function for generating a network of agents
+# @param G: A graph to add agents to the nodes of
 # @param init_score: the score to set the initial value of prisoners to
 # @param coop_vals: list of possible starting coop_probs for prisoners
 # @param coop_odds: probability of a node having the coop_prob assosciated with
 #                   the corresponding index in coop_vals
+# @return G: The input graph, with agents in G.node[u][tag]
 def addAgentsToGraph(
     G, 
     init_score=0, 
     coop_vals=[1, 0], 
-    coop_odds=[0.5, 0.5]):
+    coop_odds=[0.5, 0.5],
+    tag='agent'):
     attrs = {
         u: Node(
             score=init_score, 
@@ -27,10 +29,10 @@ def addAgentsToGraph(
             ) 
         for u in G.nodes()
     }
-    set_node_attributes(G, attrs, 'agent')
+    set_node_attributes(G, attrs, tag)
     return G
     
-def updateScores(G):
+def updateScores(G, tag='agent'):
     for u in G.nodes():
         for v in neighbors(G, u):
-            G.nodes[u]['agent'].updateScore(G.nodes[v]['agent'])
+            G.nodes[u][tag].updateScore(G.nodes[v][tag])
